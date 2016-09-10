0dfa82e8c71d0dbcdcfce5681b71b3675613e19e
Victoria Huang <vichuang@seas.upenn.edu>
2016-09-10 12:43:05 -0400

    got worldmap API to work

--- index.php
+++ index.php
@@ -6,10 +6,16 @@
   <title></title>
   <link rel="stylesheet" href="css/jquery-jvectormap-2.0.3.css" type="text/css" media="screen"/>
   <script src="https://d3js.org/d3.v4.min.js"></script>
-  <script src="js/jquery-jvectormap-2.0.3.min.js"></script>
   <script src="js/jquery-3.0.0.min.js"></script>
+  <script src="js/jquery-jvectormap-2.0.3.min.js"></script>
+  <script src="js/jquery-jvectormap-world-mill-en.js"></script>
 </head>
 <body>
-
+  <div id="world-map" style="width: 600px; height: 400px"></div>
+  <script>
+    $(function(){
+      $('#world-map').vectorMap({map: 'world_mill_en'});
+    });
+  </script>
 </body>
 </html>
