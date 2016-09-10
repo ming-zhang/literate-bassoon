53711ccafc7630ed3a5414e4125f829c2c94581e
Victoria Huang <vichuang@seas.upenn.edu>
2016-09-10 11:35:17 -0400

    fixed src locations

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title></title>
-
+  <link rel="stylesheet" href="css/jquery-jvectormap-2.0.3.css" type="text/css" media="screen"/>
   <script src="https://d3js.org/d3.v4.min.js"></script>
-  <script src="js/worldmap/jquery-jvectormap-2.0.3.min.js"></script>
+  <script src="js/jquery-jvectormap-2.0.3.min.js"></script>
   <script src="js/jquery-3.0.0.min.js"></script>
 </head>
 <body>
