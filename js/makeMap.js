57ca7ca0bfa473fafad9246e80fca326243f136e
Victoria Huang <vichuang@seas.upenn.edu>
2016-09-11 03:06:22 -0400

    debugging

--- js/makeMap.js
+++ js/makeMap.js
@@ -14,7 +14,7 @@ function emo_score() {
         url:     "../get_sentiments.php",
         success: function(data) {
             var emos = $.parseJSON(data);
-            console.log(emos);
+            console.log(data + ",1");
 /*
             for (var i = 0; i < 50; i++) {
             	for (var key in emos[i].state) {
